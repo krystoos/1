da97225a88a1194f95d113aabba50694006f22c1
krystoos <krystosika@gmail,com>
2023-08-28 22:05:38 +0200

    add inheritance

--- src/main/java/Walutomat/Calculator.java
+++ src/main/java/Walutomat/Calculator.java
@@ -4,34 +4,37 @@ public class Calculator {
     float waluta;
     float kurs2;
     float poKursie;
-
     Calculator(){
     }
     public Calculator(float waluta,float kurs,float kurs2, float poKursie) {
         this.waluta = waluta;
         this.kurs2 = kurs2;
-
     }
     public void kwotaPoProwizji(float waluta1){
         this.waluta=prowizja*waluta1;
-
     }
-    public void kwotaPoKursie(float kurs){
+    public float kwotaPoKursie(float kurs){
        this.poKursie=this.waluta*kurs;
-
+       return this.poKursie;
     }
     public float getBalance(){
         return this.poKursie;
     }
-
-    public void kwotaPierwsza(float waluta1) {
-        this.kurs2=((this.poKursie/waluta1)*prowizja);
-    }
     public float getKursPotrzebnyDoZostaniaNa0(){
         return this.kurs2;
+        }
+    public void kwotaPierwsza(float waluta1) {
+        this.kurs2=((this.poKursie/waluta1)*prowizja);
+        }
+        public float getBetterKurs(float waluta1) {
+            for (int i = 0; i < 5; i++) {
+                this.kurs2 -= 0.001;
+                System.out.println("Pomniejszony kurs: " + (this.kurs2) + "\n" + "Otrzymana kwota: " +
+                                                           this.poKursie/this.kurs2*prowizja + "nok"  );
+            }
+            return waluta1;
+        }
     }
 
-
-}
 
 
--- src/main/java/Walutomat/WalutomatEx.java
+++ src/main/java/Walutomat/WalutomatEx.java
@@ -10,13 +10,15 @@ public class WalutomatEx extends Calculator {
             Calculator calculator = new Calculator();
             Scanner scanner = new Scanner(System.in);
             float waluta1 = scanner.nextFloat();
-            calculator.kwotaPoProwizji(waluta1);
-            calculator.kwotaPierwsza(waluta1);
             Scanner scanner1 = new Scanner(System.in);
             float kurs = scanner1.nextFloat();
+            calculator.kwotaPoProwizji(waluta1);
             calculator.kwotaPoKursie(kurs);
-            System.out.println("Kwota jaką otrzymałeś po przewalutowaniu to: " +calculator.getBalance());
+            calculator.kwotaPierwsza(waluta1);
+            System.out.println("Kwota jaką otrzymałeś po przewalutowaniu to: " +calculator.getBalance() + "pln");
             System.out.println("Kurs za jaki musisz minimalnie  przelać otrzymaną kwotę aby wyjść na 0 to: " + calculator.getKursPotrzebnyDoZostaniaNa0());
+            System.out.println("Kursy korzystniejsze i odpowiednia do nich kwota jaką będziesz do przodu:");
+            calculator.getBetterKurs(waluta1);
             scanner.close();
             scanner1.close();
         } catch (Exception ignored) {
